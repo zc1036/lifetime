4a64e9a0cf908e8504c73a26027f0dc36d6f404e
Zach <dontemailme>
2018-08-11 13:19:30 -0500

    A land of gray crags, Archtrees and Everlasting Dragons.

--- today.js
+++ today.js
@@ -1,26 +1,73 @@
 
+Vue.use(Vuetify)
+
 Vue.component('log-button', {
-    template: `<div>
-                   <button class="logit-button" @click="toggleShowLogBox">Log it!</button>
-                   <div v-if="showLogNewAction" class="log-box">
-                       <div class="log-box-boxtitle">Type</div>
-                       <input id="log-box-type" type="text"></input>
-                       <div class="log-box-boxtitle">Duration</div>
-                       <vue-timepicker @change="timePicked"></vue-timepicker>
-                       <button @click="confirmLog">Confirm</button>
-                   </div>
-               </div>`,
+    template: `<v-dialog v-model="showLogNewAction" width="500">
+                 <v-btn fab absolute small right slot="activator" id="addBtn" color="info" @click="toggleShowLogBox"><v-icon>add</v-icon></v-btn>
+                 <v-card>
+                   <v-card-title class="headline grey lighten-2" primary-title>
+                     Make an entry
+                   </v-card-title>
+  
+                   <v-card-text>
+                     <v-form v-model="formIsValid">
+                       <v-text-field v-model="tags" :rules="[tagsAreValid]" solo label="Tags"></v-text-field>
+                       <v-combobox v-model="duration_h" :rules="[hrsIsValid]" :items="durations_h" label="Hours"></v-combobox>
+                       <v-combobox v-model="duration_m" :rules="[minsIsValid]" :items="durations_m" label="Minutes"></v-combobox>
+                     </v-form>
+                   </v-card-text>
+  
+                   <v-divider></v-divider>
+  
+                   <v-card-actions>
+                     <v-spacer></v-spacer>
+                     <v-btn color="error" @click="showLogNewAction = false">Cancel</v-btn>
+                     <v-btn color="info" :disabled="!formIsValid" @click="confirmLog">Confirm</v-btn>
+                   </v-card-actions>
+                 </v-card>
+               </v-dialog>`,
 
     data() {
         return {
             showLogNewAction: false,
-            duration: null
+            duration: null,
+            formIsValid: false,
+            tags: '',
+            duration_h: '0',
+            duration_m: '15',
+            durations_m: [ '15', '30', '45' ],
+            durations_h: [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10' ]
         };
     },
 
     methods: {
+        tagsAreValid(str) {
+            return str.trim().length > 0 ? true : 'Enter one or more space-separated tags';
+        },
+
+        hrsIsValid(str) {
+            if (str && str.length > 0 && isNaN(str)) {
+                return 'Enter a number';
+            }
+
+            return ((this.duration_h || 0) + (this.duration_m || 0) > 0) ? true : 'Enter some time';
+        },
+
+        minsIsValid(str) {
+            if (str && str.length > 0 && isNaN(str)) {
+                return 'Enter a number';
+            }
+
+            return ((this.duration_h || 0) + (this.duration_m || 0) > 0) ? true : 'Enter some time';
+        },
+
         toggleShowLogBox() {
             this.showLogNewAction = !this.showLogNewAction;
+        },
+
+        cancelLog() {
+            this.toggleShowLogBox();
+            
         },
 
         confirmLog() {
@@ -29,10 +76,10 @@ Vue.component('log-button', {
                 return;
             }
 
-            logTime(document.querySelectorAll('#log-box-type')[0].value
-                    .split(' ')
-                    .map((val) => val.trim())
-                    .filter((val) => val.length),
+            logTime(this.tags
+                        .split(' ')
+                        .map((val) => val.trim())
+                        .filter((val) => val.length),
                     this.duration)
                 .then(function () {
                     console.log('yes')
